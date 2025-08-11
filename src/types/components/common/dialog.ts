38e35ffabb8e684b2617c5320c572e8ebfe88a0b
boengai <keerati.khat@gmail.com>
2025-08-12 01:51:14 +0700

    🎨: common component type

--- src/types/components/common/dialog.ts
+++ src/types/components/common/dialog.ts
@@ -3,6 +3,11 @@ import type { ReactElement, ReactNode } from 'react'
 export type DialogProps = Partial<DialogVariants> & {
   children: ReactNode
   description: string
+  injected?: {
+    open: boolean
+    setOpen: (value: boolean) => void
+  }
+  onAfterClose?: () => void
   title: string
   trigger?: ReactElement<HTMLButtonElement>
 }
